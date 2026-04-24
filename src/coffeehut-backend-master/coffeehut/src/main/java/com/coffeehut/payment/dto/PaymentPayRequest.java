2eb3039d68bc2c9c27f392eca713696d9898f1a7
KaiserZheng667 <2302090168@qq.com>
2026-04-24 22:04:37 +0100

    Delete src/coffeehut-backend-master directory

--- src/coffeehut-backend-master/coffeehut/src/main/java/com/coffeehut/payment/dto/PaymentPayRequest.java
+++
@@ -1,9 +0,0 @@
-package com.coffeehut.payment.dto;
-
-import lombok.Data;
-
-@Data
-public class PaymentPayRequest {
-    private String customerID;
-    private Double transactionAmount;
-}
--- src/coffeehut-backend-master/coffeehut/src/main/java/com/coffeehut/repository/ItemRepository.java
+++
@@ -1,7 +0,0 @@
-package com.coffeehut.repository;
-
-import com.coffeehut.model.Item;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ItemRepository extends JpaRepository<Item, Long> {
-}
